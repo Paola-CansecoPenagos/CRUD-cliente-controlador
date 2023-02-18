c7cece1d17814813ee7305c7d896ff3506ee75d8
Paola-CansecoPenagos <paolaacp@gmail.com>
2023-02-18 13:27:16 -0600

    first commit

---
+++ src/user/userController.js
@@ -0,0 +1,93 @@
+var userService = require('./userServices');
+
+var createUserControllerFunc = async (req, res) =>  {
+    var result = null;
+    try {
+        result = await userService.createUserDBService(req.body);
+
+        if (result.status) {
+            res.send({ "status": true, "message": result.msg });
+        } else {
+            res.send({ "status": false, "message": result.msg });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send({ "status": false, "message": error.msg });
+    }
+}
+
+
+
+var deleteUserControllerFunc = async (req, res) => {
+  
+    try {
+        var response = await userService.deleteUserDBService(req.body);
+        console.log(response.status);
+        if (response.status) {
+            res.send({ "status": true, "message": response.msg });
+        } else {
+            res.send({ "status": false, "message": response.bodymsg });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.send({ "status": false, "message": error.msg });
+    }
+}
+
+
+var updateUserControllerFunc = async (req, res) => {
+  
+    try {
+        var response = await userService.updateUserDBService(req.body);
+        console.log(response.status);
+        if (response.status) {
+            res.send({ "status": true, "message": response.msg });
+        } else {
+            res.send({ "status": false, "message": response.bodymsg });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.send({ "status": false, "message": error.msg });
+    }
+}
+
+var loginUserControllerFunc = async (req, res) => {
+    var result = null;
+    try {
+        result = await userService.loginUserDBService(req.body);
+        if (result.status) {
+            res.send({ "status": true, "message": result.msg });
+        } else {
+            res.send({ "status": false, "message": result.msg });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.send({ "status": false, "message": error.msg });
+    }
+}
+
+
+
+var searchEmailControllerFunc = async (req, res) => {
+  
+    try {
+        var response = await userService.searchEmailDBService(req.body);
+        console.log(response.status);
+        if (response.status) {
+            res.send({ "status": true, "message": response.msg });
+        } else {
+            res.send({ "status": false, "message": response.bodymsg });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.send({ "status": false, "message": error.msg });
+    }
+}
+
+
+
+module.exports = { createUserControllerFunc, loginUserControllerFunc, searchEmailControllerFunc, deleteUserControllerFunc,updateUserControllerFunc };
---
+++ src/user/userServices.js
@@ -0,0 +1,158 @@
+var userModel = require('./userModel');
+var key = 'somekey234567884456753456';
+var encryptor = require('simple-encryptor')(key);
+
+module.exports.createUserDBService = (userDetails) => {
+
+   return new Promise(function myFn(resolve, reject)  {
+      userModel.findOne({ email: userDetails.email},function getResult(errorvalue, result) {
+         if(errorvalue) {
+            reject({status: false, msg: "Datos Invalidos"});
+         }
+         else {
+         
+            if(result !=undefined &&  result !=null) {
+
+               resolve({status: false,msg: "El email ya esta asignado a un usuario"});
+            }
+            else {
+               var userModelData = new userModel();
+
+               userModelData.firstname = userDetails.firstname;
+               userModelData.lastname = userDetails.lastname;
+               userModelData.email = userDetails.email;
+               userModelData.password = userDetails.password;
+               var encrypted = encryptor.encrypt(userDetails.password);
+               userModelData.password = encrypted;
+               
+               userModelData.save(function resultHandle(error, result) {
+               
+                  if (error) {
+                     reject({status: false,msg: "Ha habido un error al momento de crear el usuario"});
+               } else {
+                     resolve({status: true,msg: "El usuario ha sido creado"});
+               }
+               });
+      
+            }
+         }
+      })
+   });
+}
+
+module.exports.deleteUserDBService = (userDetails) => {
+
+   return new Promise(function myFn(resolve, reject)  {
+      userModel.findOne({ email: userDetails.email},function getresult(errorvalue, result) {
+         if(errorvalue) {
+            reject({status: false, msg: "Datos Invalidos"});
+         }
+         else {
+
+            if(result !=undefined &&  result !=null) {
+               console.log(result);
+               if( result.email == userDetails.email) {
+
+                  userModel.deleteOne({ email: userDetails.email }, function (err) {
+                     if (err) return handleError(err);
+                   });
+                  resolve({status: true,msg: "El usuario ha sido eliminado"});
+               }
+               else {
+                  reject({status: false,msg: "Email no encontrado"});
+               }
+            }
+            else {
+               reject({status: false,msg: "Detalles de usuario invalido"});
+            }
+           
+         }
+      });
+   });
+}
+
+module.exports.updateUserDBService = (userDetails)=>{
+   return new Promise(function myFn(resolve, reject)  {
+      userModel.findOneAndUpdate(
+         {email: userDetails.email},
+         {$set:{
+            password: encryptor.encrypt(userDetails.password),
+            firstname: userDetails.firstname,
+            lastname: userDetails.lastname
+            }
+         },
+         {new: true},
+         function getresult(errorvalue, result) {
+         if(errorvalue) {
+            reject({status: false, msg: "Datos Invalidos"});
+         }
+         else {
+            if(result !=undefined &&  result !=null) {
+               
+
+               if(result.email = userDetails.email) {
+                  resolve({status: true,msg: "El usuario ha sido modificado"});
+               }
+            }
+            else {
+               reject({status: false,msg: "Datos de usuario invalido"});
+            }
+         }
+      });
+   });
+}
+
+
+module.exports.loginUserDBService = (userDetails)=>  {
+   return new Promise(function myFn(resolve, reject)  {
+      userModel.findOne({ email: userDetails.email},function getresult(errorvalue, result) {
+         if(errorvalue) {
+            reject({status: false, msg: "Datos Invalidos"});
+         }
+         else {
+            if(result !=undefined &&  result !=null) {
+               var decrypted = encryptor.decrypt(result.password);
+
+               if(decrypted== userDetails.password) {
+                  resolve({status: true,msg: "Usuario Validado"});
+               }
+               else {
+                  reject({status: false,msg: "Falla en validacion de usuario"});
+               }
+            }
+            else {
+               reject({status: false,msg: "Detalles de usuario invalido"});
+            }
+         }
+      });
+   });
+}
+
+
+module.exports.searchEmailDBService = (userDetails)=>  {
+   return new Promise(function myFn(resolve, reject)  {
+      userModel.findOne({ email: userDetails.email},function getresult(errorvalue, result) {
+         if(errorvalue) {
+            reject({status: false, msg: "Datos Invalidos"});
+         }
+         else {
+         
+            if(result !=undefined &&  result !=null) {
+               console.log(result);
+
+               if( result.email == userDetails.email) {
+                  resolve({status: true,msg: "El email '" + userDetails.email + "' ha sido encontrado dentro del registro de usuarios"});
+               }
+               else {
+                  reject({status: false,msg: "Email no encontrado"});
+               }
+            }
+            else {
+               reject({status: false,msg: "Detalles de usuario invalido"});
+            }
+         }
+      });
+   });
+}
+
+
